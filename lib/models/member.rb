b99f90094e6fea8d60380db5919162bd866e6b52
mnavoy4 <mnavoy4@gmail.com>
2020-07-08 11:01:35 -0600

    Basic Main Menu Complete

--- lib/models/member.rb
+++ lib/models/member.rb
@@ -1,8 +1,28 @@
+require 'tty-prompt'
+
 class Member < ActiveRecord::Base
+  $prompt = TTY::Prompt.new
   has_many :gym_sessions
   has_many :workouts, through: :gym_sessions
 
   def self.create_new
-    puts "You have created your profile"
+    puts "What is your name"
+    name = gets.chomp
+    goal = $prompt.select("What is your goal") do |menu|
+      menu.choice 'Losing weight'
+      menu.choice 'Gaining strength'
+    end
+    new_user = Member.create(:name=>name, :goal=>goal)
+    new_user
+    puts "Nice to meet you #{name}! Let's start working towards #{goal}!"
+    puts "Please note your member ID: #{new_user.id}. You will need it to view your profile later!"
+  end
+
+  def self.see_user_profile
+    puts "Please enter your member ID"
+    member_id = gets.chomp
+    found_user = self.find_by id: member_id
+    puts "Name: #{found_user[:name]}"
+    puts "Goal: #{found_user[:goal]}"
   end
 end
--- runner.rb
+++ runner.rb
@@ -2,14 +2,31 @@ require_relative 'config/environment'
 require 'pry'
 require 'tty-prompt'
 
-prompt = TTY::Prompt.new
+
+# prompt = TTY::Prompt.new
 app = Cli.new
 app.welcome
-# app.main_menu
-if app.main_menu == 'Build new profile'
-  app.new_user
+loop do
+  case app.main_menu
+  when 'Build new profile'
+    app.new_user
+  when 'Select your workout'
+    app.select_type_of_workout
+    app.workout
+    # bar = TTY::ProgressBar.new("Work out in progress [:bar]", total: 30)
+    # 30.times do
+    #   sleep(0.1)
+    #   bar.advance(1)
+    # end
+    # app.completed_workout
+  when 'See user profile'
+    app.see_user_profile
+  when 'Exit GitFit'
+    app.end_app
+    break
+  end
 end
-app.select_type_of_workout
+
 
 
 
