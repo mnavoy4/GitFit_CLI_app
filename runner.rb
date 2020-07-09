9e57efbf205cacc2cd5bad30ef78b1d65aa892e3
mnavoy4 <mnavoy4@gmail.com>
2020-07-09 08:34:56 -0600

    Refactored methods, added delete profile feature, added edit profile feature

--- runner.rb
+++ runner.rb
@@ -9,10 +9,14 @@ loop do
   when 'Build new profile'
     app.new_member
   when 'Select your workout'
-    app.select_type_of_workout app.sign_in
+    app.select_type_of_workout
     app.workout
   when 'See member profile'
     app.see_member_profile
+  when 'Edit member profile'
+    app.edit_member_profile
+  when 'Delete member profile'
+    app.delete_member_profile
   when 'Exit GitFit'
     app.end_app
     break
